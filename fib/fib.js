1d9003048ec92278bf887e3db939e7aa92946ead
timon0596 <timon0596@yandex.ru>
2018-09-19 12:39:37 +0300

    completed

---
+++ fib/fib.js
@@ -0,0 +1,20 @@
+
+function fib(n){
+	var c,a=1,b=1;
+	if(n<3){
+		return 1;
+	}else{
+	for(var i=2;i<n;i++){
+	 c=(a+b);
+	 a=b;
+	 b=c;
+	}}
+	return c;
+}
+function changehtml(){
+	answer.innerHTML = v.value + "-e число Фибоначчи = " + fib(v.value)
+}
+var v = document.getElementById("fib");
+var r = document.getElementById("res");
+var answer = document.getElementById("output");
+r.onclick = function (){ changehtml()}
